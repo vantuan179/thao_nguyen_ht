90ab9868033b7eefbce621ef1c294f5ea51ca80e
vantuan179 <tuan.phanvan179@zuno.vc>
2026-08-14 15:26:49 +0700

    update

--- math_kids_jdbc_project/src/main/java/com/kidsmath/controller/QuizRestController.java
+++ math_kids_jdbc_project/src/main/java/com/kidsmath/controller/QuizRestController.java
@@ -3,10 +3,12 @@ package com.kidsmath.controller;
 import com.kidsmath.model.Quiz;
 import com.kidsmath.model.User;
 import com.kidsmath.service.QuizService;
+
+import jakarta.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import javax.servlet.http.HttpSession;
 import java.util.HashMap;
 import java.util.Map;
 
--- math_kids_jdbc_project/src/main/java/com/kidsmath/controller/UserController.java
+++ math_kids_jdbc_project/src/main/java/com/kidsmath/controller/UserController.java
@@ -8,13 +8,15 @@ import com.kidsmath.service.GradeService;
 import com.kidsmath.service.LessonService;
 import com.kidsmath.service.QuizService;
 import com.kidsmath.service.UserService;
+
+import jakarta.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import javax.servlet.http.HttpSession;
 import java.util.List;
 
 @Controller
